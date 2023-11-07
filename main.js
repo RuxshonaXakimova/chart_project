5156fa12bee1d37379c19bec6b6ee6020a024d46
ruxshonaxakimova <ruxshonaxakimova774@gmail.com>
2023-11-07 17:53:38 +0500

    fixed2

--- main.js
+++ main.js
@@ -1,93 +1,35 @@
 import { header_create } from "./modules/ui";
 import { coin_reload } from "./modules/ui";
+import { arr_info } from "./db";
 
 let box = document.querySelector('.box')
+let open_menu_btn = document.querySelector('.open_menu_btn')
+let menu_main = document.querySelector('.menu_main')
+let close_menu_btn = document.querySelector('.menu_main button')
+let box_one_p = document.querySelectorAll('.box_one_top_left p')
+console.log(box_one_p);
+
 
 header_create()
-
-let arr_info = [
-    {
-        name:'BitCoin',
-        img: './img/Component 1.png',
-        balance: '600 gRC',
-        price: '$30,000,000',
-        changes: [
-            {
-                fromDollar: '0,074 grc',
-                toDollar: '$6 542, 35'
-            },
-            {
-                fromDollar: '0,034 grc',
-                toDollar: '$6 264, 35'
-            },
-            {
-                fromDollar: '0,075 grc',
-                toDollar: '$6 642, 22'
-            }
-        ]
-    },
-    {
-        name:'GridCoin',
-        img: './img/Component 1.png',
-        balance: '600 gRC',
-        price: '$30,000,000',
-        changes: [
-            {
-                fromDollar: '0,074 grc',
-                toDollar: '$6 542, 35'
-            },
-            {
-                fromDollar: '0,034 grc',
-                toDollar: '$6 264, 35'
-            },
-            {
-                fromDollar: '0,075 grc',
-                toDollar: '$6 642, 22'
-            }
-        ]
-    },
-    {
-        name:'Ethereum',
-        img: './img/Component 1.png',
-        balance: '600 gRC',
-        price: '$30,000,000',
-        changes: [
-            {
-                fromDollar: '0,074 grc',
-                toDollar: '$6 542, 35'
-            },
-            {
-                fromDollar: '0,034 grc',
-                toDollar: '$6 264, 35'
-            },
-            {
-                fromDollar: '0,075 grc',
-                toDollar: '$6 642, 22'
-            }
-        ]
-    },
-    {
-        name:'Aeternity',
-        img: './img/Component 1.png',
-        balance: '600 gRC',
-        price: '$30,000,000',
-        changes: [
-            {
-                fromDollar: '0,074 grc',
-                toDollar: '$6 542, 35'
-            },
-            {
-                fromDollar: '0,034 grc',
-                toDollar: '$6 264, 35'
-            },
-            {
-                fromDollar: '0,075 grc',
-                toDollar: '$6 642, 22'
-            }
-        ]
-    }
-]
-
-
-
 coin_reload(arr_info, box)
+
+
+open_menu_btn.onclick = () =>{
+    menu_main.classList.remove('close_menu_class')
+    menu_main.classList.add('open_menu_class')
+}
+
+close_menu_btn.onclick = () => {
+    menu_main.classList.remove('open_menu_class')
+    menu_main.classList.add('close_menu_class')
+}
+
+box_one_p.forEach(text => {
+    text.onclick = () => {
+        box_one_p.forEach(item=>{
+            item.classList.remove('choosed_p')
+        })
+
+        text.classList.add('choosed_p')
+    }
+})
--- modules/analytics.js
+++ modules/analytics.js
@@ -2,47 +2,48 @@ import Chart from 'chart.js/auto';
 
 let ctx = document.querySelector('#myChart')
 
-const MONTHS = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ];
 
-export function months(config) {
-    var cfg = config || {};
-    var count = cfg.count || 12;
-    var section = cfg.section;
-    var values = [];
-    var i, value;
-  
-    for (i = 0; i < count; ++i) {
-      value = MONTHS[Math.ceil(i) % 12];
-      values.push(value.substring(0, section));
-    }
-  
-    return values;
-  }
+// const MONTHS = [
+//     'January',
+//     'February',
+//     'March',
+//     'April',
+//     'May',
+//     'June',
+//     'July',
+//     'August',
+//     'September',
+//     'October',
+//     'November',
+//     'December'
+//   ];
 
-const labels = months({count: 7});
+// export function months(config) {
+//     var cfg = config || {};
+//     var count = cfg.count || 12;
+//     var section = cfg.section;
+//     var values = [];
+//     var i, value;
+
+//     for (i = 0; i < count; ++i) {
+//       value = MONTHS[Math.ceil(i) % 12];
+//       values.push(value.substring(0, section));
+//     }
+
+//     return values;
+//   }
+
+// const labels = months({count: 7});
 
 const data = {
-  labels: labels,
+  labels: [2, 4, 6, 8, 10, 12, 14],
   datasets: [{
     label: 'My First Dataset',
     data: [65, 59, 80, 81, 56, 70, 90],
     fill: {
       target: 'origin',
-                above: 'rgb(0, 151, 232, 0.20)',   // Area will be red above the origin
-                below: 'rgb(1, 143, 255, 0.00)'
+      above: 'rgb(0, 151, 232, 0.20)',   // Area will be red above the origin
+      below: 'rgb(1, 143, 255, 0.00)'
     },
     borderColor: '#018FFF',
     tension: 0.4,
@@ -55,73 +56,82 @@ const chart_line = new Chart(ctx, {
   type: 'line',
   data: data,
   options: {
-  elements: {
-    point: {
-      backgroundColor: 'blue',
-      borderWidth: 5
+    elements: {
+      point: {
+        backgroundColor: 'blue',
+        borderWidth: 5
+      }
+    },
+    elements: {
+      bar: {
+        tension: 5,
+        backgroundColor: 'blue',
+        borderWidth: 5
+      }
     }
-  },
-  elements: {
-    bar: {
-      tension: 5,
-      backgroundColor: 'blue',
-      borderWidth: 5
-    }
-  }
-    
+
   }
 });
 
-const config = {
-    type: 'line',
-    data: data,
-    options: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: 'Chart.js Line Chart - Cubic interpolation mode'
-          },
-        },
-        interaction: {
-          intersect: false,
-        },
-        scales: {
-          x: {
-            display: true,
-            title: {
-              display: true
-            }
-          },
-          y: {
-            display: true,
-            title: {
-              display: true,
-              text: 'Value'
-            },
-            suggestedMin: -10,
-            suggestedMax: 200
-          }
-        }
-      },
-  };
+
+
+let ctx1 = document.querySelector('#first_chart')
+
+const data_doughnut = {
+  labels: [
+    'Blue',
+    'Yellow',
+    'White',
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'blue',
+      'Yellow',
+      'white'
+    ],
+    hoverOffset: 4,
+    borderWidth: 0,
+    hoverBorderWidth: 5
+  }]
+};
+
+const doughnutChart = new Chart(ctx1, {
+  type: 'doughnut',
+  data: data_doughnut,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+let ctx2 = document.querySelector('#maket_canvas')
+
+const labels = ['NOV 15', 'NOV 16', 'NOV 17', 'NOV 18']
+
+const data_maket = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [8000, 4000, 10000, 8000, 2000],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.4
+  }]
+};
+
+const myChart = new Chart(ctx2, {
+  type: 'line',
+  data: data_maket,
+});
 
 
 
-  // const chart_doughnut = new Chart(ctx, {
-  //   type: 'doughnut',
-  //   data: data,
-  //   options: {
-  //     onClick: (e) => {
-  //       const canvasPosition = getRelativePosition(e, chart);
-  
-  //       // Substitute the appropriate scale IDs
-  //       const dataX = chart_doughnut.scales.x.getValueForPixel(canvasPosition.x);
-  //       const dataY = chart_doughnut.scales.y.getValueForPixel(canvasPosition.y);
-  //     }
-      
-  //   }
-  // });
 
 
 
